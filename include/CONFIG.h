911b2c15e5368f8b233739b3034a73deb87b0087
PanNakhum <60010839@kmitl.ac.th>
2022-10-07 11:12:59 +0700

    main

---
+++ include/CONFIG.h
@@ -0,0 +1,16 @@
+// WiFi
+// #define WIFI_SSID "SSID"
+// #define WIFI_PASSWORD "psk1234"
+#define WIFI_SSID "Hello_2.4G"
+#define WIFI_PASSWORD "Pan1234!"
+
+// Thingsboard platform
+#define THINGS_BOARD_NAME "ESP_1"
+#define THINGS_BOARD_HOST "senseme.ddns.net"
+#define THINGS_BOARD_PORT 1883
+#define THINGS_BOARD_TOKEN "0T1CeC6ypQO1hqU8FW2f"
+#define THINGS_BOARD_TOPIC "v1/devices/me/telemetry"
+
+// DS18B20
+#define DS18B20_1 16 // For DS18B20 Channel 1.
+#define DS18B20_2 17 // For DS18B20 Channel 2.
---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+#include<WiFi.h>
+#include <PubSubClient.h>
+#include "../include/CONFIG.h"
+#include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+OneWire oneWire1(DS18B20_1);
+OneWire oneWire2(DS18B20_2);
+
+DallasTemperature sensors1(&oneWire1);
+DallasTemperature sensors2(&oneWire2);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // setup serial
+
+  // Start the DS18B20 sensor 1 & 2
+  sensors1.begin();
+  sensors2.begin();
+
+  Serial.print("Connecting to "); // Connect wifi
+  Serial.println(WIFI_SSID);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  mqtt.setServer(THINGS_BOARD_HOST,THINGS_BOARD_PORT);
+}
+
+void loop() {
+  if (mqtt.connected() == false) {
+    Serial.print("MQTT connection... ");
+    if (mqtt.connect(THINGS_BOARD_NAME, THINGS_BOARD_TOKEN, THINGS_BOARD_TOKEN)) {
+      Serial.println("connected");
+    } else {
+      Serial.println("failed");
+      delay(5000);
+    }
+  } else {
+    mqtt.loop();
+  }
+
+  sensors1.requestTemperatures(); 
+  float temperature1 = sensors1.getTempCByIndex(0);
+  sensors2.requestTemperatures(); 
+  float temperature2 = sensors2.getTempCByIndex(0);
+  
+  char jsonS[200];
+  // sprintf(jsonS,"{\"temperature1\":%f,\"temperature2\":%f}",random(5000)/ 100.0,random(5000)/ 100.0);
+  sprintf(jsonS,"{\"temperature1\":%f,\"temperature2\":%f}",temperature1,temperature2);
+  Serial.println(jsonS);
+  mqtt.publish(THINGS_BOARD_TOPIC, jsonS);
+  delay(2000);
+
+}
